90cb1d5bb5ddf201e647905ea115026c9e8d6878
George Tsnobiladze <115263426+Georgetsn29@users.noreply.github.com>
2025-06-15 00:53:12 +0400

    Add files via upload

--- js/button.js
+++ js/button.js
@@ -2,10 +2,8 @@ const button = document.getElementById('showButton');
 const hiddenText = document.getElementById('hiddenText');
 
 button.addEventListener('click', () => {
-  // Toggle the text visibility
   hiddenText.classList.toggle('show');
 
-  // Update the button label
   if (hiddenText.classList.contains('show')) {
     button.textContent = 'აკეცეთ';
   } else {
